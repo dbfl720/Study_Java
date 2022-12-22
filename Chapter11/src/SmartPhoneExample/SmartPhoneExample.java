a4418c0e52c910e22c4601908a35c009dd4253a0
홍유리 <116433637+dbfl720@users.noreply.github.com>
2022-12-22 23:54:59 +0900

    Add files via upload

---
+++ Chapter11/src/SmartPhoneExample/SmartPhoneExample.java
@@ -0,0 +1,17 @@
+package SmartPhoneExample;
+
+import SmartPhone.SmartPhone;
+
+public class SmartPhoneExample {
+
+	public static void main(String[] args) {
+		SmartPhone myPhone = new SmartPhone("구글", "안드로이드");
+		
+		String strObj = myPhone.toString();
+		System.out.println(strObj);
+		
+		System.out.println(myPhone);
+
+	}
+
+}
---
+++ Chapter11/src/toString/toString.java
@@ -0,0 +1,14 @@
+package toString;
+
+import java.util.Date;
+
+public class toString {
+
+	public static void main(String[] args) {
+		Object obj1 = new Object();
+		Date obj2 = new Date();
+		System.out.println(obj1.toString());
+		System.out.println(obj2.toString());
+	}
+
+}
