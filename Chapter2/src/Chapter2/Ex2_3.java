17ecd34e5d429df986dd9e9f404020975c8bc8e5
홍유리 <116433637+dbfl720@users.noreply.github.com>
2022-12-25 20:47:09 +0900

    자바의 정석 Chapter2
    자바의 정석 Chapter2 (1 ~ 10) 예제 공부

---
+++ Chapter2/src/Chapter2/Ex2_3.java
@@ -0,0 +1,12 @@
+package Chapter2;
+
+public class Ex2_3 {
+
+	public static void main(String[] args) {
+		System.out.println(5+3);
+		System.out.println(5-3);
+		System.out.println(5*3);
+		System.out.println(5/3);
+	}
+
+}
---
+++ Chapter2/src/Chapter2/Ex2_5.java
@@ -0,0 +1,14 @@
+package Chapter2;
+
+public class Ex2_5 {
+
+	public static void main(String[] args) {
+		int x = 10;
+		int y = 5;						//x,y의 값이 바뀌어도 변경하지 않아도 된다.
+		System.out.println(x+y);
+		System.out.println(x-y);
+		System.out.println(x*y);
+		System.out.println(x/y);
+	}
+
+}
